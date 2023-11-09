f766e56fe303e58a58055793afbd9187ca8a3273
Leopoldo Bernardes <leopoldobms@gmail.com>
2023-11-09 08:42:08 -0300

    endentação pipedream

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <?php 
 
     $handle = curl_init('https://enpcqbxz5w4tdsw.m.pipedream.net');
+    $data = ['url' => $requestUrl, 'headers'=> $headers, 'request' => $jsonRequest, 'response' => utf8_encode($result)];
+    $encodedData = json_encode($data);
+    curl_setopt($handle, CURLOPT_POST, 1);
+    curl_setopt($handle, CURLOPT_POSTFIELDS, $encodedData);
+    curl_setopt($handle, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
 
-        $data = ['url' => $requestUrl, 'headers'=> $headers, 'request' => $jsonRequest, 'response' => utf8_encode($result)];
-
-        $encodedData = json_encode($data);
-
-        curl_setopt($handle, CURLOPT_POST, 1);
-        curl_setopt($handle, CURLOPT_POSTFIELDS, $encodedData);
-        curl_setopt($handle, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-
-        $resultado = curl_exec($handle);
+    $resultado = curl_exec($handle);
